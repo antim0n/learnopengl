e327d52bd1da7a905b6dbcc7201b1a5475670d2b
Simon <sb875@students.uni-freiburg.de>
2024-04-25 15:13:53 +0200

    complete for transfer

--- Main.cpp
+++ Main.cpp
@@ -8,6 +8,15 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+// simplest vertex shader, no transformation needed
+const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+
 int main()
 {
     glfwInit();
@@ -31,6 +40,31 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
+    // vertex shader setup
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    float vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+             0.5f, -0.5f, 0.0f,
+             0.0f,  0.5f, 0.0f
+    };
+    unsigned int VBO; // vertex buffer object
+    glGenBuffers(1, &VBO); // generate one buffer, can store a large number of vertices in the GPU's memory
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); // current buffer for GL_ARRAY_BUFFER
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // copy vertex data into the buffer's memory
+
 
     // render loop, 1 iteration is 1 frame
     while (!glfwWindowShouldClose(window))
